e86a7b84e955491de3c6426fd0a19133289f0695
momen-arabi <momenarabi25@gmail.com>
2025-01-28 21:13:36 +0200

    Project Setup

---
+++ src/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FriendItem from "./FriendItem";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <ul>
+        <FriendItem />
+      </ul>
+    </div>
+  );
+}
